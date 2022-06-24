43c7eab72ce9c523e68dad5a4451187e2725bc5c
gallardo <aiki.hosogaya@gmail.com>
2022-06-24 17:36:15 +0900

    add routing

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,11 +9,10 @@ import { MessageService }    from '../message.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  selectedHero?: Hero;
-
-  heroesList: Hero[] = [];
+  heroes: Hero[] = [];
 
   constructor(
     private heroService: HeroService,
@@ -24,13 +23,8 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes()
-      .subscribe(heroes => this.heroesList = heroes);
+      .subscribe(heroes => this.heroes = heroes);
   }
 }
